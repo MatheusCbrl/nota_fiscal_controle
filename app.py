e1eb8d2a3b62b01be675d269087dd1b99777b99b
Matheus Cabral <48923315+MatheusCbrl@users.noreply.github.com>
2025-01-09 15:55:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,30 @@ def init_supabase() -> Client:
 
 supabase = init_supabase()
 
+
+# Função para inserir dados na tabela cadastro_remetente
+def inserir_remetente(nome, cpf_cnpj, endereco, contato):
+    data = {
+        "nome": nome,
+        "cpf_cnpj": cpf_cnpj,
+        "endereco": endereco,
+        "contato": contato
+    }
+    response = supabase.table("cadastro_remetente").insert(data).execute()
+    return response
+
+
+# Função para buscar todos os remetentes cadastrados
+def buscar_remetentes():
+    response = supabase.table("cadastro_remetente").select("*").execute()
+    return response.data if response else []
+
+# Função para buscar todos as ordens para impressão
+def buscar_ordens_impressao():
+    response = supabase.table("ordens_despacho").select("*").execute()
+    return response.data if response else []
+
+
 # Função para salvar ordem no Supabase
 def salvar_ordem(data):
     try:
@@ -48,14 +72,12 @@ def mostrar_login():
         else:
             st.error("Usuário ou senha incorretos")
 
-
 class PDF(FPDF):
-    def header(self):
-        # Logotipo
-        self.image("logomarca.png", 10, 8, 33)  # Substitua pelo caminho do seu logotipo
-        self.set_font("Arial", "B", 12)
-        self.cell(0, 10, "Relatório do Cliente", ln=True, align="C")
-        self.ln(10)
+    #def header(self):
+    #    # Logotipo
+    #    self.image("logomarca.png", 150, 8, 50)  # Substitua pelo caminho do seu logotipo
+    #    self.set_font("Arial", "B", 12)
+    #    self.ln(10)
 
     def footer(self):
         self.set_y(-15)
@@ -63,60 +85,40 @@ class PDF(FPDF):
         self.cell(0, 10, f"Página {self.page_no()}", align="C")
 
     def add_title(self, cliente):
-        self.set_font("Arial", "B", 16)
+        self.set_font("Arial", "B", 12)
         self.set_text_color(40, 40, 100)
+        
+        self.image("logomarca.png", 150, 8, 50)  # Substitua pelo caminho do seu logotipo
         self.cell(0, 10, f"Relatório: {cliente}", ln=True, align="C")
-        self.ln(10)
+        self.ln(5)
+        
+        # Inserindo a segunda imagem
+        self.image("logomarca.png", 150, 350, 50)
 
-    def add_details(self, df_cliente, colunas_mapeadas):
-        self.set_font("Arial", size=10)
+    def add_details(self, df_cliente, colunas_mapeadas, y_offset=0):
+        self.set_y(y_offset)  # Definir o deslocamento vertical
+        self.set_font("Arial", size=12) 
         for index, row in df_cliente.iterrows():
-            self.set_fill_color(230, 230, 230)  # Fundo cinza claro
-            self.cell(0, 10, f"Ordem #{index + 1}", ln=True, align="L", fill=True)
-            self.ln(2)
-
             for col, value in row.items():
                 if col in colunas_mapeadas:
                     self.set_font("Arial", "B", 10)
                     self.cell(50, 10, f"{colunas_mapeadas[col]}:", border=0, ln=0)
-                    self.set_font("Arial", "", 10)
+                    self.set_font("Arial", "", 8)
                     self.cell(0, 10, str(value), border=0, ln=1)
             self.ln(5)  # Espaço entre ordens
-            
-# Função para gerar PDF estilizado com mapeamento
-def gerar_pdf(cliente, df_cliente):
-    # Mapeamento das colunas para os nomes desejados
-    colunas_mapeadas = {
-        "remetente": "Remetente",
-        "endereco_remetente": "Endereço do Remetente",
-        "destinatario": "Destinatário",
-        "endereco_destinatario": "Endereço do Destinatário",
-        "cidade": "Cidade",
-        "frete_tipo": "Tipo de Frete",
-        "valor_frete": "Valor do Frete",
-        "data": "Data",
-        "hora": "Hora",
-        "conteudo": "Conteúdo",
-        "qtde_volumes": "Quantidade de Volumes",
-        "valor_nf": "Valor da Nota Fiscal",
-        "peso": "Peso (Kg)",
-        "solicitado_por": "Solicitado Por"
-    }
 
-    pdf = PDF()
-    pdf.set_auto_page_break(auto=True, margin=15)
-    pdf.add_page()
-
-    # Título
-    pdf.add_title(cliente)
-
-    # Detalhes linha por linha
-    pdf.add_details(df_cliente, colunas_mapeadas)
-
-    # Salvar PDF
-    arquivo_pdf = f"Relatorio_{cliente.replace(' ', '_')}.pdf"
-    pdf.output(arquivo_pdf)
-    return arquivo_pdf
+# Função para alternar o estado de "Pago" para "A Pagar" e vice-versa
+def alternar_frete_tipo(ordem_id, estado_atual):
+    novo_estado = "A Pagar" if estado_atual == "Pago" else "Pago"
+    try:
+        response = supabase.table("ordens_despacho").update({"frete_tipo": novo_estado}).eq("id", ordem_id).execute()
+        if response:
+            st.success(f"Estado alterado com sucesso para: {novo_estado}")
+        else:
+            st.error("Erro ao atualizar o estado. Tente novamente.")
+    except Exception as e:
+        st.error(f"Erro ao atualizar o estado: {e}")
+        
 
 # Verificar autenticação
 if 'authenticated' not in st.session_state:
@@ -125,19 +127,36 @@ if 'authenticated' not in st.session_state:
 if not st.session_state.authenticated:
     mostrar_login()
 else:
-    aba = st.sidebar.radio("Navegação", ["Cadastro", "Consulta e Relatórios", "Filtrar por Cliente"])
+    aba = st.sidebar.radio("Navegação", ["Cadastro","Cadastro de Remetentes", "Consulta e Relatórios", "Filtrar por Cliente"])
     # Aba de Cadastro
-    
     if aba == "Cadastro":
         # Layout do Streamlit
         st.title("Sistema de Ordem de Despacho")
         
+        # Pesquisa de Remetente para Cadastro de Notas
+        st.subheader("Pesquisar Remetente")
+        remetentes = buscar_remetentes()
+        remetente_selecionado = None
+
+        if remetentes:
+            remetente_selecionado = st.selectbox(
+                "Selecione o Remetente", 
+                options=remetentes, 
+                format_func=lambda x: f"{x['nome']}"
+            )
+        else:
+            st.warning("Nenhum remetente cadastrado ainda!")
+
+        # Preenchimento automático do Remetente
+        remetente_nome = remetente_selecionado['nome'] if remetente_selecionado else ""
+        remetente_endereco = remetente_selecionado['endereco'] if remetente_selecionado else ""
+
         # Campos do formulário
         col1, col2, col3 = st.columns(3)
         with col1:
-            remetente = st.text_input("Remetente")
+            remetente = st.text_input("Remetente", value=remetente_nome, disabled=False)
         with col2:
-            endereco_remetente = st.text_input("Endereço do Remetente")
+            endereco_remetente = st.text_input("Endereço do Remetente", value=remetente_endereco, disabled=False)
         with col3:
             destinatario = st.text_input("Destinatário")
         col1, col2, col3 = st.columns(3)
@@ -190,19 +209,27 @@ else:
             response = salvar_ordem(dados_ordem)
             if response:
                 st.success("Ordem salva com sucesso!")
+    # aba "Cadastro de Remetentes"
+    elif aba == "Cadastro de Remetentes":
+        st.header("Cadastro de Remetente")
+        with st.form("form_cadastro_remetente"):
+            nome = st.text_input("Nome (Obrigatório)", max_chars=255)
+            cpf_cnpj = st.text_input("CPF/CNPJ (Opcional)", max_chars=20)
+            endereco = st.text_area("Endereço (Opcional)")
+            contato = st.text_input("Contato (Opcional)", max_chars=50)
         
-        # Exibir registros existentes
-        #st.subheader("Ordens de Despacho Registradas")
-        #try:
-        #    ordens = supabase.table("ordens_despacho").select("*").execute()
-        #    if ordens.data:
-        #        st.table(ordens.data)
-        #    else:
-        #        st.info("Nenhuma ordem registrada ainda.")
-        #except Exception as e:
-        #    st.error(f"Erro ao buscar dados: {e}")
-    
-    # Aba de Consulta e Relatórios
+            submitted = st.form_submit_button("Cadastrar")
+        
+            if submitted:
+                if not nome:
+                    st.error("O campo Nome é obrigatório!")
+                else:
+                    response = inserir_remetente(nome, cpf_cnpj if cpf_cnpj else None ,endereco if endereco else None, contato if contato else None)
+                    if response:
+                        st.success("Remetente cadastrado com sucesso!")
+                    else:
+                        st.error(f"Erro ao cadastrar remetente: {response.json()}")
+    # Atualizando a aba "Consulta e Relatórios"
     elif aba == "Consulta e Relatórios":
         st.header("Consulta e Relatórios de Ordens de Despacho")
         # Buscar dados do banco
@@ -227,11 +254,16 @@ else:
             # Calcular faturamento
             faturamento_total = df_filtrado["valor_frete"].sum()
             st.write(f"**Faturamento Total Filtrado:** R$ {faturamento_total:.2f}")
-
-            # Tabela de resultados filtrados
+        
+            # Tabela de resultados filtrados com botão para alternar estado
             st.subheader("Resultados Filtrados")
-            st.dataframe(df_filtrado)
-
+            for index, row in df_filtrado.iterrows():
+                st.write(f"**Ordem ID:** {row['id']}")
+                st.write(f"**Remetente:** {row['remetente']}")
+                st.write(f"**Tipo de Frete:** {row['frete_tipo']}")
+                if st.button(f"Alterar para {'Pago' if row['frete_tipo'] == 'A Pagar' else 'A Pagar'}", key=row['id']):
+                    alternar_frete_tipo(row['id'], row['frete_tipo'])
+        
             # Gráfico: Fretes Pagos vs Não Pagos
             st.subheader("Gráfico: Fretes Pagos vs Não Pagos")
             if "frete_tipo" in df_filtrado.columns:
@@ -244,38 +276,74 @@ else:
         st.header("Filtrar e Imprimir Informações por Cliente")
     
         # Buscar dados do banco
+        clientes = buscar_ordens_impressao()
         df = buscar_ordens()
-    
-        if not df.empty:
-            # Dropdown para selecionar cliente
-            cliente_selecionado = st.selectbox("Selecione um Cliente (Remetente)", options=df["remetente"].unique())
-    
-            # Filtrar dados do cliente
-            df_cliente = df[df["remetente"] == cliente_selecionado]
-    
-            # Exibir informações do cliente
-            st.subheader(f"Informações do Cliente: {cliente_selecionado}")
-            st.dataframe(df_cliente)
-    
-            # Botão para gerar PDF
-            if st.button("Gerar PDF"):
-                if not df_cliente.empty:
-                    arquivo_pdf = gerar_pdf(cliente_selecionado, df_cliente)
-                    st.success(f"PDF gerado com sucesso: {arquivo_pdf}")
-                    
-                    # Download do arquivo gerado
-                    with open(arquivo_pdf, "rb") as pdf_file:
-                        st.download_button(
-                            label="Baixar PDF",
-                            data=pdf_file,
-                            file_name=arquivo_pdf,
-                            mime="application/pdf",
-                        )
-                else:
-                    st.warning("Nenhum dado disponível para o cliente selecionado.")
-        else:
-            st.info("Nenhuma ordem registrada no momento.")
-    
+        
+        # Permitir seleção de até dois clientes
+        clientes_selecionados = st.multiselect(
+            "Selecione até dois clientes para gerar o relatório:",
+            options=clientes,
+            format_func=lambda x: f"{x['remetente']}",
+            max_selections=2
+        )
+
+        if st.button("Gerar PDF"):
+            if len(clientes_selecionados) == 0:
+                st.warning("Por favor, selecione pelo menos um cliente.")
+            elif len(clientes_selecionados) > 2:
+                st.warning("Selecione no máximo dois clientes.")
+            else:
+                # Gerar dados filtrados para cada cliente
+                pdf = PDF(format="A4")
+                pdf.set_auto_page_break(auto=True)
+                for idx, cliente in enumerate(clientes_selecionados):
+                    df_cliente = buscar_ordens()
+                    df_cliente_filtrado = df_cliente[df_cliente["remetente"] == cliente["remetente"]]
+
+                    if df_cliente_filtrado.empty:
+                        st.warning(f"Não há dados disponíveis para o cliente {cliente['remetente']}.")
+                        continue
+
+                    # Adicionar meia página para o cliente
+                    if idx % 2 == 0:  # Adicionar nova página se for o primeiro cliente de uma dupla
+                        pdf.add_page()
+
+                    # Adicionar dados do cliente à página atual
+                    max_altura = 148.5  # Meia altura de uma página A4
+                    pdf.add_title(cliente["remetente"])
+                    pdf.add_details(df_cliente_filtrado, colunas_mapeadas = {
+                                                            "remetente": "Remetente",
+                                                            "endereco_remetente": "Endereço do Remetente",
+                                                            "destinatario": "Destinatário",
+                                                            "endereco_destinatario": "Endereço do Destinatário",
+                                                            "cidade": "Cidade",
+                                                            "frete_tipo": "Tipo de Frete",
+                                                            "valor_frete": "Valor do Frete",
+                                                            "data": "Data",
+                                                            "hora": "Hora",
+                                                            "conteudo": "Conteúdo",
+                                                            "qtde_volumes": "Quantidade de Volumes",
+                                                            "valor_nf": "Valor da Nota Fiscal",
+                                                            "peso": "Peso (Kg)",
+                                                            "solicitado_por": "Solicitado Por"
+                                                        }, y_offset=(idx % 2) * max_altura)
+
+            # Salvar PDF único
+            arquivo_pdf_unico = "Relatorio_Clientes_Unico.pdf"
+            pdf.output(arquivo_pdf_unico)
+
+            st.success("PDF único gerado com sucesso!")
+            with open(arquivo_pdf_unico, "rb") as pdf_file:
+                st.download_button(
+                    label="Baixar PDF Único",
+                    data=pdf_file,
+                    file_name=arquivo_pdf_unico,
+                    mime="application/pdf",
+                )
+                
+
+
+
 with st.sidebar:
       st.image("./logomarca.bmp")
       st.markdown(
